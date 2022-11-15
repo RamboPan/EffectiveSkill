e3684bab32128b6c02323b7cafac80be57feed26
RamboPan <RamboPan@yeah.net>
2022-11-15 20:14:09 +0800

    add listFiles readme and useage

--- 1_ListFiles/listFiles.sh
+++ 1_ListFiles/listFiles.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #输出的文件名
-file="files.txt"
+basepath=$(cd `dirname $0`; pwd)
+file=$basepath/"files.txt"
 
 #检测如果带一个参数运行，列出该目录，其他情况列出当前目录
 if [[ $# == 1 ]] ;then
     ls $1 > $file
 else
-    ls > $file
+    ls $basepath > $file
 fi
