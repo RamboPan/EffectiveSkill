05e3e20f4b2586b5b7d17b6234f4c52d4c668c39
RamboPan <keyi.pan@megatronix.co>
2023-10-18 14:29:27 +0800

    update comment

--- 1_ListFiles/listFiles.bat
+++ 1_ListFiles/listFiles.bat
@@ -1,10 +1,10 @@
 @echo off
 
-::指定输出文件名
+:指定输出文件名
 set basePath=%~dp0
 set fileName=%basePath%files.txt
 
-::如果带一个参数运行，则列出该路径下所有文件,如果不带参数运行，则直接输出当前目录
+:如果带一个参数运行，则列出该路径下所有文件,如果不带参数运行，则直接输出当前目录
 if Not "%1" == "" (
     dir "%1" /B > %fileName%
 ) else (
